4b470061f68729835c77bc94a9187285346587dc
Hendi Santika <hendisantika@gmail.com>
2018-08-01 20:21:47 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/thymeleafdecorator/ThymeleafDecoratorApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.thymeleafdecorator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafDecoratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafDecoratorApplication.class, args);
+    }
+}
